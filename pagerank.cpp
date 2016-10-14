c652fd01dc3779675f0f49d3497d81ddda95787e
Clive Chan <cc@clive.io>
2016-10-14 15:55:37 -0400

    Working surfer model

--- pagerank.cpp
+++ pagerank.cpp
@@ -17,43 +17,49 @@ std::mt19937_64 gen(rd());
 
 class Page{
 private:
-  double rank;
+  int visits = 0;
   vector<Page*> links;
 public:
-  Page(double initialRank){rank = initialRank;}
-  Page(const Page& other) : links(other.links){
-    rank = other.rank;
-  }
-  double getRank(){return rank;}
+  Page(){}
+  int resetVisits(){visits = 0;}
+  int getVisits(){return visits;}
+  void visit(){visits++;}
   void linkTo(Page& p){
     links.push_back(&p);
   }
-  Page& getRandomPage(){
+  Page* getRandomPage(){
     uniform_int_distribution<int> dist(0, links.size()-1);
-    return *links[dist(gen)]; //subscript operator > deref operator
+    return links[dist(gen)]; //precedence: subscript operator > deref operator
   }
 };
 
+void pageRank(vector<Page> www){
+  uniform_int_distribution<int> distWWW(0, www.size()-1);
+  uniform_real_distribution<double> dist01(0.0,1.0);
+  
+  const int iterations = 10000;
+  const double delta = 1.0;
+  Page* currentPage = &www[distWWW(gen)];
+  
+  for(int i = 0; i < iterations; i++){
+    currentPage->visit();
+    //cout << currentPage->getVisits() << " ";
+    if(dist01(gen) < delta)
+      currentPage = currentPage->getRandomPage();
+    else
+      currentPage = &www[distWWW(gen)];
+  }
+}
+
 int main(){
-  const int size = 3;
-  vector<Page> www(size, 1.0/size);
+  vector<Page> www(3);
   www[0].linkTo(www[1]);
   www[0].linkTo(www[2]);
   www[1].linkTo(www[2]);
   www[2].linkTo(www[0]);
-  uniform_int_distribution<int> distWWW(0, www.size()-1);
-  uniform_real_distribution<double> dist01(0.0,1.0);
   
-  const int iterations = 1000;
-  const double delta = 0.2;
-  Page& currentPage = www[distWWW(gen)];
+  pageRank(www);
   
-  for(int i = 0; i < iterations; i++)
-    if(dist01(gen) < delta)
-      currentPage = currentPage.getRandomPage();
-    else
-      currentPage = www[distWWW(gen)];
-  
-  for(int i = 0; i < size; i++)
-    cout << i << ": " << www[i].getRank() << endl;
+  for(int i = 0; i < www.size(); i++)
+    cout << i << ": " << www[i].getVisits() << endl;
 }
