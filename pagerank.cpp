fa7b087fbe67e92621de6c5416764c170a1e9d96
Clive Chan <cc@clive.io>
2016-10-28 15:28:42 -0400

    updated pagerank but doesn't really work - sigh cpp random

--- pagerank.cpp
+++ pagerank.cpp
@@ -2,18 +2,16 @@
 A simple implementation of PageRank.
   * pagerank is useful because we have all these relations 
   between pages, and it would be nice to solve for all these 
-  reputation contributions exactly, but inverting 1Mx1M matrices
-  is hard.
+  reputation contributions exactly, but finding eigenvalues 
+  for big matrices is hard.
 */
 
 #include <iostream>
 #include <vector>
-#include <random>
+#include "randutils.hpp"
 using namespace std;
 
-//singleton rng
-std::random_device rd;
-std::mt19937_64 gen(rd());
+randutils::mt19937_rng rng;
 
 class Page{
 private:
@@ -28,26 +26,22 @@ public:
     links.push_back(&p);
   }
   Page* getRandomPage(){
-    uniform_int_distribution<int> dist(0, links.size()-1);
-    return links[dist(gen)]; //precedence: subscript operator > deref operator
+    return links[rng.uniform(0, links.size()-1)];
   }
 };
 
 void pageRank(vector<Page> www){
-  uniform_int_distribution<int> distWWW(0, www.size()-1);
-  uniform_real_distribution<double> dist01(0.0,1.0);
-  
   const int iterations = 10000;
   const double delta = 1.0;
-  Page* currentPage = &www[distWWW(gen)];
+  Page* currentPage = &www[rng.uniform(0, www.size()-1)];
   
   for(int i = 0; i < iterations; i++){
     currentPage->visit();
     //cout << currentPage->getVisits() << " ";
-    if(dist01(gen) < delta)
+    if(rng.uniform(0.0, 1.0) < delta)
       currentPage = currentPage->getRandomPage();
     else
-      currentPage = &www[distWWW(gen)];
+      currentPage = &www[rng.uniform(0, www.size()-1)];
   }
 }
 
