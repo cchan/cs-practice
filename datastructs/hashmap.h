c4fa0efa20c4984e5ecccf1ac2ed32f9b87c0f85
Clive Chan <cc@clive.io>
2017-02-06 18:47:49 -0500

    starting hashmap

---
+++ datastructs/hashmap.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <cstdlib>
+#include <stdexcept>
+#include <iostream>
+
+template <typename T>
+class hashmap{
+  
+};
+
+//have to explicitly instantiate hash functions for certain types
